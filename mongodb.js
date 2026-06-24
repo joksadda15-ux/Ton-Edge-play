1b33f0f4cfbcd664fbad5c6c6e6424b63aa7fed2
joksadda15-ux <joksadda15@gmail.com>
2026-06-24 07:40:39 +0600

    Add MongoDB client connection setup

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+
+let client;
+let clientPromise;
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri);
+  global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
+
+export async function getDb() {
+  const c = await clientPromise;
+  return c.db('tonedge');
+}
+
+export default clientPromise;
